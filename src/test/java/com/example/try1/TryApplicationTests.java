a584bb81164092642800c8d8e9abfee6075a6052
Manar <manar@Manars-MacBook-Air.local>
2021-06-08 04:54:17 +0300

    Initial commit

---
+++ src/test/java/com/example/try1/TryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.try1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
